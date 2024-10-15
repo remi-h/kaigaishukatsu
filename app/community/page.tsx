11cf5318a777d93b7287d20cfdb905deb8bab0ff
Rémi <59444214+remi-h@users.noreply.github.com>
2024-10-15 21:00:06 +0200

    update

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -5,16 +5,13 @@ export default function Community() {
     return (
         <main>
             <h1>COMMUNITY</h1>
-            <h2>WORK ABROAD on DISCORD</h2>
-            <p>2024年5月にオープンした新しいコミュニティです。</p>
+            <h2>WORK ABROAD Coming Soon</h2>
             <p>対象者：現地就職を目指している方・現地就職をされた方</p>
             <p>コミュニテイ運営：情報交換やオンラインミートアップ・場所が合えばオフラインイベントも開催する予定です。</p>
-            <div className="max-w-[500px]">
-                <Link href="https://discord.gg/edJbrWNQBW" target="_blank">
-                    <IconBox icon="🌐" title="WORK ABROAD COMMUNITY on Discord" description="" />
-                </Link>
-            </div>
-            
+            <p> Waitlist 登録 </p>
+            <form>
+                <input type="email" />
+            </form>
         </main>
     )
 }
